c63cb32b471c2e65a021ff081810100077e56f99
Gabriel <gabrieltemiscancino@g.horrycountyschools.net>
2019-03-26 10:09:54 -0400

    Strategy name
    Always betray

--- team4.py
+++ team4.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'RX-7 Squad' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'Betray'
+strategy_description = 'Always betray'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
