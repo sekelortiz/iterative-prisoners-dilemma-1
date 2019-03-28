045eb399df36bc7ed953a1fb8930a0dd8e5dd2e3
sekelortiz <atsekelortiz@g.horrycountyschools.net>
2019-03-28 10:01:51 -0400

    mission impossible
    Attempt 3

--- team2.py
+++ team2.py
@@ -19,7 +19,7 @@ def move(my_history, their_history, my_score, their_score):
     '''
 
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
-    # their_history: a string of the same length as history, possibly empty. 
+    # their_history: a string of hehehe the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
     # The most recent round is my_history[-1] and their_history[-1].
     
